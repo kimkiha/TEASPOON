60ea9a1f653a5505f381132e9b5f51fa41c9dacd
hjbrains2 <63648985+hjbrains2@users.noreply.github.com>
2020-04-23 17:11:55 +0900

    store-wish_생성_하정

---
+++ teaspoonProject/src/com/teaspoon/member/controller/MypageWish.java
@@ -0,0 +1,55 @@
+package com.teaspoon.member.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.teaspoon.member.model.service.MemberService;
+import com.teaspoon.store.model.vo.Product;
+
+/**
+ * Servlet implementation class MypageWish
+ */
+@WebServlet("/wishList.me")
+public class MypageWish extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MypageWish() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int pcode = Integer.parseInt(request.getParameter("pcode"));
+		
+		ArrayList<Product> list = new MemberService().selectWishList(pcode);
+		request.setAttribute("list", list);
+		
+		RequestDispatcher view = request.getRequestDispatcher("views/mypage/mypage_wish.jsp");
+		view.forward(request, response);
+	
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
