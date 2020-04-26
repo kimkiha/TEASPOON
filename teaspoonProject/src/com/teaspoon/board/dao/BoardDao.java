4727d1ad03ceb38a5a159011903562383e813053
KIHA <flklc901@gmail.com>
2020-04-27 00:46:44 +0900

    board_dao수정_기하
    board_dao수정_기하

--- teaspoonProject/src/com/teaspoon/board/dao/BoardDao.java
+++ teaspoonProject/src/com/teaspoon/board/dao/BoardDao.java
@@ -65,7 +65,7 @@ public class BoardDao {
 		int result = 0;
 		
 		PreparedStatement pstmt = null;
-		String sql =prop.getProperty("insertAttachemnt");
+		String sql =prop.getProperty("insertMagazineAttachemnt");
 		
 		try {
 			pstmt = conn.prepareStatement(sql);
